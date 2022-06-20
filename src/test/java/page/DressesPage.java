03854007d1b6a07579d4b93ff8036f27f3521da7
MarinaKrasyuk <MarinaKrasyuk@coherentsolutions.com>
2022-06-20 10:43:28 +0300

    20.06.2022

--- src/test/java/page/DressesPage.java
+++ src/test/java/page/DressesPage.java
@@ -8,30 +8,17 @@ import test.BaseTest;
 
 public class DressesPage extends BaseTest {
 
-    private By product1 = By.xpath("(//*[@class='product_img_link'])[1]");
-    private By product2 = By.xpath("(//*[@class='product_img_link'])[2]");
-    private By product3 = By.xpath("(//*[@class='product_img_link'])[3]");
+    private String product_str="(//*[@class='product_img_link'])";
 
     public DressesPage(WebDriver driver, WebDriverWait wait) {
         this.driver = driver;
         this.wait = wait;
     }
-
-    public ProductPage clickProduct1() {
-        wait.until(ExpectedConditions.visibilityOfElementLocated(product1));
-        driver.findElement(product1).click();
+    public ProductPage clickProduct(int i) {
+        By product=By.xpath(product_str+"["+i+"]");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(product));
+        driver.findElement(product).click();
         return new ProductPage(driver, wait);
     }
 
-    public ProductPage clickProduct2() {
-        wait.until(ExpectedConditions.visibilityOfElementLocated(product2));
-        driver.findElement(product2).click();
-        return new ProductPage(driver, wait);
-    }
-
-    public ProductPage clickProduct3() {
-        wait.until(ExpectedConditions.visibilityOfElementLocated(product3));
-        driver.findElement(product3).click();
-        return new ProductPage(driver, wait);
-    }
 }
--- src/test/java/test/AddToCartTest.java
+++ src/test/java/test/AddToCartTest.java
@@ -10,27 +10,27 @@ import page.ProductPage;
 import java.text.DecimalFormat;
 
 
-public class AddToCartTest extends BaseTest{
-    Double total=0.0;
+public class AddToCartTest extends BaseTest {
+    Double total = 0.0;
     private static DecimalFormat df2 = new DecimalFormat("#.00");
+
     @Test
-    public void addToCartTest(){
-        AccountPage accountPage=login();
-        DressesPage dressesPage=accountPage.clickDressesPage();
-        ProductPage productPage=dressesPage.clickProduct1();
-        total+=productPage.clickCartButton();
-        dressesPage=productPage.clickDressesPage();
-        productPage=dressesPage.clickProduct2();
-        total+=productPage.clickCartButton();
-        dressesPage=productPage.clickDressesPage();
-        productPage=dressesPage.clickProduct3();
-        total+=productPage.clickCartButton();
+    public void addToCartTest() {
+        AccountPage accountPage = login();
+        ProductPage productPage = null;
+        DressesPage dressesPage = accountPage.clickDressesPage();
+        for (int i = 1; i < 4; i++) {
+            productPage = dressesPage.clickProduct(i);
+            total += productPage.clickCartButton();
+            dressesPage = productPage.clickDressesPage();
+
+        }
+
         CartPage cartPage = productPage.clickCarts();
-        Double totalExpected= Double.valueOf(df2.format(total+2.0));
-        Double totalPrice=cartPage.getTotalPrice();
+        Double totalExpected = Double.valueOf(df2.format(total + 2.0));
+        Double totalPrice = cartPage.getTotalPrice();
 
         Assertions.assertTrue(totalExpected.equals(totalPrice));
-
 
 
     }
